2984c3de225774c57abdc533743a4911f4b2ce32
CiReal <Kydan301193@gmail.com>
2025-01-17 08:35:58 +0700

    m

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import { useState } from 'react'
 
 function App() {
-  //useState
-  const orders = [100, 200, 300];
-  const [counter, setCounter] = useState(() => {
-    const total = orders.reduce((total, cur) => total + cur);
-    return total;
-  });
-  const handleIncrease = () => {
-    setCounter(prevState => prevState + 1);
-    setCounter(prevState => prevState + 1);
-    setCounter(prevState => prevState + 1);
-  }
-
-  const [info, setInfo] = useState({
-    name: "Nguyen Van A",
-    age: 18,
-    address: "Ha Noi"
-  });
-  const handleUpdate = () => {
-    // setInfo({
-    //   ...info,
-    //   bio: "yeu mau hong"
-    // });
-    setInfo(prev => ({
-      ...prev,
-      bio: "Yeu mau hong"
-    }));
-  }
   return (
-    <div className="App">
-      <h1>{counter}</h1>
-      <h1>
-        {JSON.stringify(info)}
-      </h1>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleUpdate}>Update</button>
+    <div className="App" style={{ padding: 32 }}>
+      Hello anh em
     </div>
   );
 }
