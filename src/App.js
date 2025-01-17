93e61284e3235f57289c25ed2007c7e73836d7db
CiReal <Kydan301193@gmail.com>
2025-01-17 08:33:49 +0700

    Mount-UnMount

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import { useState } from 'react'
+import Content from './Content';
 
 function App() {
-
-  const [job, setJob] = useState('');
-  const [jobs, setJobs] = useState(() => {
-    const storageJobs = JSON.parse(localStorage.getItem('jobs'));//nếu cho hàm này ra ngoài sẽ gây lãng phí vì render lại nhiều lần 
-    return storageJobs ?? [];
-  });
-  const handleSubmit = () => {
-    if (job !== '' && !jobs.includes(job)) {
-      setJobs(prev => {
-        const newJobs = [...prev, job];
-        const jsonJobs = JSON.stringify(newJobs);
-
-        localStorage.setItem('jobs', jsonJobs);//lưu dữ liệu vào localStorage để refresh lại vẫn có dữ liệu với key là jobs
-        return newJobs;
-      });
-    }
-    setJob('');
-  }
+  const [show, setShow] = useState(false);
   return (
     <div className="App" style={{ padding: 32 }}>
-      <input
-        value={job}
-        onChange={e => setJob(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Add</button>
-
-      <ul>
-        {
-          jobs.map((job, index) => (
-            <li key={index}>{job}</li>
-          ))
-        }
-      </ul>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <Content />}
     </div>
   );
 }
