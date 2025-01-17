f748a6224c0cc527e49afd35bcf59e7fbecb5845
CiReal <Kydan301193@gmail.com>
2025-01-17 22:14:34 +0700

    commit

--- src/UseEffect2.js
+++ src/UseEffect2.js
@@ -1,21 +1,25 @@
 import { useEffect, useState } from "react";
 function UseEffect2() {
-  const [width, setWidth] = useState(window.innerWidth);
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    }
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    }
-  }, []);
+  const [countdown, setCountdown] = useState(180);
+  //c1
 
+  // useEffect(() => {
+  //   const timerId = setTimeout(() => {
+  //     setCountdown(countdown - 1);
+  //   }, 1000);
+  //   return ()=> clearTimeout(timerId);
+  // }, [countdown]);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCountdown(prev => prev - 1);
+    }, 1000);
+    return () => clearInterval(timerId);
+  }, []);
   return (
     <div>
-      <h1>{width}</h1>
+      <h1>{countdown}</h1>
     </div>
   )
-
 }
 export default UseEffect2
