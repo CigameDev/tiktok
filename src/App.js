54e88c369cb020d5b0e25e6e59510786bbd7c9b9
CiReal <Kydan301193@gmail.com>
2025-01-17 17:06:09 +0700

    m

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import { useState } from 'react'
-import Content from './Content';
 import UseEffect2 from './UseEffect2';
 function App() {
   const [show, setShow] = useState(false);
   const [showPosts, setShowPosts] = useState(false);
   return (
     <div className="App" style={{ padding: 32 }}>
-      <button onClick={() => setShow(!show)}>Toggle</button>
-      {show && <Content />}
-
-      <br />
       <button onClick={() => setShowPosts(!showPosts)}>Show Posts</button>
       {showPosts && <UseEffect2 />}
-
-
     </div>
   );
 }
--- src/UseEffect2.js
+++ src/UseEffect2.js
@@ -1,20 +1,34 @@
 import { useEffect, useState } from "react";
 
 function UseEffect2() {
+  const tabs = ['posts', 'comments', 'albums'];
   const [title, setTitle] = useState('');
   const [posts, setPosts] = useState([]);
+  const [type, setType] = useState('posts');
+
+  console.log(type);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
       .then(posts => {
-        console.log(posts);
-        setPosts(posts);
+        //setPosts(posts);
       })
   }, []);//đây là trường hợp truyền thêm mảng ,nếu không có hoặc không sử dụng useEffect thì sẽ callAPI rất nhiều lần =>lag
 
   return (
     <div>
+      {tabs.map(tab => (
+        <button
+          key={tab}
+          style={type === tab ? {
+            color: '#fff',
+            backgroundColor: '#333'
+          } : {}}
+          onClick={() => setType(tab)}>{tab}
+        </button>
+      ))}
+
       <input
         value={title}
         onChange={e => setTitle(e.target.value)}
