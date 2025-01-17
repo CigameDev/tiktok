ea993905ea27dd3408250c0b83b7f882cdcb71cd
CiReal <Kydan301193@gmail.com>
2025-01-17 21:37:06 +0700

    m

--- src/UseEffect2.js
+++ src/UseEffect2.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 function UseEffect2() {
   const tabs = ['posts', 'comments', 'albums'];//bài toán là có các nút bấm,bấm vào nút nào thì call API để show ra dữ liệu
-  const [posts, setPosts] = useState([]);
-  const [type, setType] = useState('posts');
+  const [posts, setPosts] = useState([]); //posts Lưu mảng dữ liệu call về từ API
+  const [type, setType] = useState('posts');//type lưu chữ 1 trong các dữ liệu của tabs
+  const [showGoToTop, setShowToTop] = useState(false);
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${type}`)
@@ -11,6 +12,18 @@ function UseEffect2() {
         setPosts(p);
       })
   }, [type]);
+
+  useEffect(() => {
+
+    const handleScroll = () => {
+      setShowToTop(window.scrollY >= 300);
+    }
+    window.addEventListener('scroll', handleScroll);
+    //cleanup function
+    return () => {
+      window.removeEventListenerEventListener('scroll', handleScroll);
+    }
+  }, [])
 
   return (
     <div>
@@ -30,6 +43,15 @@ function UseEffect2() {
           <li key={post.id}>{post.title || post.name}</li>
         ))}
       </ol>
+      {showGoToTop && (
+        <button style={{
+          position: 'fixed',
+          right: 20,
+          bottom: 20
+        }}>
+          Go to Top
+        </button>
+      )}
     </div>
   )
 
