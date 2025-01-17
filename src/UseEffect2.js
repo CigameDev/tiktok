3b4fffb9be3226469d4231cc8a056468481f40c8
CiReal <Kydan301193@gmail.com>
2025-01-17 21:52:08 +0700

    update

--- src/UseEffect2.js
+++ src/UseEffect2.js
@@ -1,57 +1,19 @@
 import { useEffect, useState } from "react";
 function UseEffect2() {
-  const tabs = ['posts', 'comments', 'albums'];//bài toán là có các nút bấm,bấm vào nút nào thì call API để show ra dữ liệu
-  const [posts, setPosts] = useState([]); //posts Lưu mảng dữ liệu call về từ API
-  const [type, setType] = useState('posts');//type lưu chữ 1 trong các dữ liệu của tabs
-  const [showGoToTop, setShowToTop] = useState(false);
-
+  const [width, setWidth] = useState(window.innerWidth);
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
-      .then(res => res.json())
-      .then(p => {
-        setPosts(p);
-      })
-  }, [type]);
-
-  useEffect(() => {
-
-    const handleScroll = () => {
-      setShowToTop(window.scrollY >= 300);
+    const handleResize = () => {
+      setWidth(window.innerWidth);
     }
-    window.addEventListener('scroll', handleScroll);
-    //cleanup function
+    window.addEventListener('resize', handleResize);
     return () => {
-      window.removeEventListenerEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
     }
-  }, [])
+  }, []);
 
   return (
     <div>
-      {tabs.map(tab => (
-        <button
-          key={tab}
-          style={type === tab ? {
-            color: '#fff',
-            backgroundColor: '#333'
-          } : {}}
-          onClick={() => setType(tab)}>{tab}
-        </button>
-      ))}
-
-      <ol>
-        {posts.map(post => (
-          <li key={post.id}>{post.title || post.name}</li>
-        ))}
-      </ol>
-      {showGoToTop && (
-        <button style={{
-          position: 'fixed',
-          right: 20,
-          bottom: 20
-        }}>
-          Go to Top
-        </button>
-      )}
+      <h1>{width}</h1>
     </div>
   )
 
