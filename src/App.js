d2ac1b8cd58b6ed65c5548eaf7887deb18b08e11
CiReal <Kydan301193@gmail.com>
2025-01-17 08:23:00 +0700

    todolist-localstorage

--- src/App.js
+++ src/App.js
@@ -1,92 +1,39 @@
 import { useState } from 'react'
 
 function App() {
-  const gifts = [
-    "CPU i9",
-    "RAM 32G RGB",
-    "RGB Keyboard"
-  ]
-  const [gift, setGift] = useState();
-  const handleGift = () => {
-    const index = Math.floor(Math.random() * gifts.length);
-    setGift(gifts[index]);
-  }
-  /*
-    Two-way binding
-    1 chiều là chiều tương tác trên giao diện
-    2 là chiều chữ liệu
-  */
-  const [name, setName] = useState("");
-  const courses = [
-    {
-      id: 1,
-      name: "HTML,CSS"
-    },
-    {
-      id: 2,
-      name: "Javascript"
-    },
-    {
-      id: 3,
-      name: "ReactJs"
-    }
-  ]
-  const [checked, setChecked] = useState(2);
-  const handleSubmit = () => {
-    //Call API
-    console.log({ id: checked });
-  }
 
-  const [checkedbox, setChekedBox] = useState([]);
-  const handleSubmitCheckBox = () => {
-    //Call API 
-    //In ra mảng các phần tử được chọn
-    console.log(checkedbox);
-  }
-  const OnChangeCheckedBox = (id) => {
-    setChekedBox(prev => {
-      if (prev.includes(id)) {
-        return prev.filter(item => item !== id);
-      }
-      else {
-        return [...prev, id];
-      }
-    });
+  const [job, setJob] = useState('');
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'));//nếu cho hàm này ra ngoài sẽ gây lãng phí vì render lại nhiều lần 
+    return storageJobs ?? [];
+  });
+  const handleSubmit = () => {
+    if (job !== '' && !jobs.includes(job)) {
+      setJobs(prev => {
+        const newJobs = [...prev, job];
+        const jsonJobs = JSON.stringify(newJobs);
+
+        localStorage.setItem('jobs', jsonJobs);//lưu dữ liệu vào localStorage để refresh lại vẫn có dữ liệu với key là jobs
+        return newJobs;
+      });
+    }
+    setJob('');
   }
   return (
     <div className="App" style={{ padding: 32 }}>
-      <h1>{gift || "Chưa có phần thưởng"}</h1>
-      <button onClick={handleGift}>Lấy thưởng</button>
-      <br />
       <input
-        value={name}
-        onChange={e => setName(e.target.value)}
+        value={job}
+        onChange={e => setJob(e.target.value)}
       />
-      <button onClick={() => setName("Nguyen Van B")}>Change Name</button>
-      {/* ở đây ấn button mặc dù name đã đổi thành Nguyễn Văn B nhưng trên giao diện chưa đổi  nếu không có value={name}*/}
-      {courses.map(course => (
-        <div key={course.id}>
-          <input
-            type='radio'
-            checked={checked === course.id}
-            onChange={() => setChecked(course.id)}
-          />
-          {course.name}
-        </div>
-      ))}
-      <button onClick={handleSubmit}>Register radio</button>
-      <br />
-      {courses.map(course => (
-        <div key={course.id}>
-          <input
-            type='checkbox'
-            checked={checkedbox.includes(course.id)}
-            onChange={() => OnChangeCheckedBox(course.id)}
-          />
-          {course.name}
-        </div>
-      ))}
-      <button onClick={handleSubmitCheckBox}>Register CheckBox</button>
+      <button onClick={handleSubmit}>Add</button>
+
+      <ul>
+        {
+          jobs.map((job, index) => (
+            <li key={index}>{job}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
